85bb3c6949b8b0ccf6cb6402082eef1a399bb83c
Francis Aiello <fran.aiello@dev9.com>
2015-12-02 20:36:10 -0800

    Added new method to reverse each element in string array Added method Javadocs for clarity

--- src/main/java/com/interview/questions/SplitArrayApart.java
+++ src/main/java/com/interview/questions/SplitArrayApart.java
@@ -2,6 +2,7 @@ package com.interview.questions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class SplitArrayApart {
@@ -13,14 +14,11 @@ public class SplitArrayApart {
      * to split up the array into separate individual arrays
      *
      * @param array
-     * @param splitInto
-     *
-     * Example: array [1,2,3,4,5,6]
-     *          returns [1,2] [3,4] [5,6]
+     * @param splitInto Example: array [1,2,3,4,5,6]
+     *                  returns [1,2] [3,4] [5,6]
      */
     public static List<int[]> splitIntegerArray(int[] array, int splitInto) {
-        int arrayLength = array.length;
-        int subArraySize = arrayLength / splitInto;
+        int subArraySize = array.length / splitInto;
         List<int[]> list = new ArrayList<int[]>();
 
         for (int i = 0; i <= array.length - 1; i = i + subArraySize) {
@@ -39,15 +37,56 @@ public class SplitArrayApart {
      * @param splitInto
      * @return
      */
-    public static List<String[]> splitStringArray(String[] array, int splitInto) {
-        int arrayLength = array.length;
-        int subArraySize = arrayLength / splitInto;
+    public static List<String[]> splitStringArray(String[] array, int splitInto, boolean reverse) {
+        int subArraySize = array.length / splitInto;
         List<String[]> list = new ArrayList<String[]>();
+        //List<String[]> reverseList = new ArrayList<String[]>();
 
         for (int i = 0; i <= array.length - 1; i = i + subArraySize) {
             list.add(Arrays.copyOfRange(array, i, i + subArraySize));
         }
 
+        // shall we reverse the order of sub arrays?
+        if (reverse) {
+            List<String[]> reverseList = reverseList(list);
+            return reverseList;
+        }
+
         return list;
+    }
+
+    /**
+     * Accepts a list of string arrays and returns a reverse order
+     *
+     * @param list
+     * @return
+     */
+    private static List<String[]> reverseList(List<String[]> list) {
+        List<String[]> reverseList = new ArrayList<String[]>();
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            reverseList.add(list.get(i));
+        }
+
+        return reverseList;
+    }
+
+    /**
+     * Accepts a list of string arrays and reverses each element within
+     * string array and returns new list
+     *
+     * @param list
+     * @return
+     */
+    public static List<String[]> reverseElementsInEachStringArray(List<String[]> list) {
+        List<String[]> reverseList = new ArrayList<String[]>();
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            String[] array = list.get(i);
+            Collections.reverse(Arrays.asList(array));
+            reverseList.add(array);
+        }
+
+        return reverseList;
     }
 }
--- src/test/java/com/interview/questions/SplitArrayApartTest.java
+++ src/test/java/com/interview/questions/SplitArrayApartTest.java
@@ -2,6 +2,7 @@ package com.interview.questions;
 
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,6 +13,10 @@ public class SplitArrayApartTest {
 
     // Split original array into halves
     int splitIntoHalves = 2;
+
+    // Reverse the order of the sub-lists returned
+    boolean reverse = false;
+
 
     @Test
     public void splitIntegerArray_IntoThirds() {
@@ -27,14 +32,29 @@ public class SplitArrayApartTest {
 
     @Test
     public void splitStringArray_IntoThirds() {
-        List<String[]> list = SplitArrayApart.splitStringArray(new String[]{"a", "b", "c", "d", "e", "f"}, splitIntoThirds);
+        List<String[]> list = SplitArrayApart.splitStringArray(new String[]{"a", "b", "c", "d", "e", "f"}, splitIntoThirds, reverse);
         assertThat(list).containsExactly(new String[]{"a", "b"}, new String[]{"c", "d"}, new String[]{"e", "f"});
     }
 
     @Test
+    public void splitStringArray_IntoThirds_ReturnReverseOrder() {
+        List<String[]> list = SplitArrayApart.splitStringArray(new String[]{"a", "b", "c", "d", "e", "f"}, splitIntoThirds, reverse = true);
+        assertThat(list).containsExactly(new String[]{"e", "f"}, new String[]{"c", "d"}, new String[]{"a", "b"});
+    }
+
+    @Test
     public void splitStringArray_IntoHalves() {
-        List<String[]> list = SplitArrayApart.splitStringArray(new String[]{"a", "b", "c", "d", "e", "f"}, splitIntoHalves);
+        List<String[]> list = SplitArrayApart.splitStringArray(new String[]{"a", "b", "c", "d", "e", "f"}, splitIntoHalves, reverse);
         assertThat(list).containsExactly(new String[]{"a", "b", "c"}, new String[]{"d", "e", "f"});
     }
 
+    @Test
+    public void reverseElementsInEachStringArray() {
+        List<String[]> input = new ArrayList<String[]>();
+        input.add(new String[]{"a", "b"});
+        input.add(new String[]{"c", "d"});
+        input.add(new String[]{"e", "f"});
+        List<String[]> list = SplitArrayApart.reverseElementsInEachStringArray(input);
+        assertThat(list).containsExactly(new String[]{"f", "e"}, new String[]{"d", "c"}, new String[]{"b", "a"});
+    }
 }
